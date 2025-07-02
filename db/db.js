381acf6d457ffcc6d861b9376c9fb96f8fcbb6d3
MaxtorUwU <129220633+MaxtorUwU@users.noreply.github.com>
2025-07-01 22:44:29 -0400

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,28 @@
+/* const { Pool } = require('pg');
+require('dotenv').config();
+console.log('DB_HOST:', process.env.DB_HOST);
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = pool; */
+
+
+
+  const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+module.exports = pool; 
